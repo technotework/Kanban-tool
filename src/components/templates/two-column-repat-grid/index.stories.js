249b6d9d63244a300761ada3210a3ae2cb3e29b7
capeknote <capeknote@MacBookAir.local>
2020-02-06 10:42:56 +0900

    templates/TwoColumnRepeatGrid

---
+++ src/components/templates/two-column-repat-grid/index.stories.js
@@ -0,0 +1,30 @@
+import {
+    createDefStory,
+    createStory,
+    tagTemp
+} from "@/components/utils/story-creator"
+import TwoColumnRepeatGrid from "./index.vue";
+
+let description = 'import TwoColumnRepeatGrid from "@/components/templates/two-column-repat-grid/"';
+export default createDefStory("templates/TwoColumnRepeatGrid", TwoColumnRepeatGrid, description);
+
+//---------------------
+//Basic
+let setting = {
+    name: "TwoColumnRepeatGrid",
+    compos: {
+        TwoColumnRepeatGrid: TwoColumnRepeatGrid
+    },
+    props: {},
+    action: {},
+    template: tagTemp `<TwoColumnRepeatGrid ${'props'} ${'action'}>
+        <div style="background-color:#ff0; width:100%; margin-right:5px; ">1</div>
+        <div style="background-color:#ff0; width:100%; margin-right:5px;">2</div>
+        <div style="background-color:#ff0; width:100%;  margin-right:5px; ">3</div>
+        <div style="background-color:#ff0; width:100%; margin-right:5px;">4</div>
+        <div style="background-color:#ff0; width:100%;  margin-right:5px; ">5</div>
+        <div style="background-color:#ff0; width:100%; margin-right:5px;">6</div>
+    </TwoColumnRepeatGrid>`
+};
+
+export const Basic = () => (createStory(setting));
---
+++ src/components/templates/two-column-repat-grid/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div :class="$style.grid">
+    <slot />
+  </div>
+</template>
+
+<script>
+import base from "@/components/utils/base-mixin";
+
+export default {
+  mixins: [base],
+  name: "TwoColumnRepeatGrid",
+  props: {}
+};
+</script>
+<style lang="scss" module>
+.grid {
+  display: grid;
+  grid-template-columns: 35% 65%;
+  grid-template-rows: 36px;
+}
+</style>
+
+
