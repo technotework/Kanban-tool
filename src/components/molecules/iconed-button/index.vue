7f51a6db9cc207d7e8840d2b7885e39b5b5d4fdf
technote <>
2020-02-09 15:54:13 +0900

    IconedButton

---
+++ src/components/molecules/iconed-button/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <PrimaryButton @click="onClick" :class="$style.button">
+    <IconS :type="type" :class="$style.icon" />
+  </PrimaryButton>
+</template>
+
+<script>
+import IconedText from "@/components/molecules/iconed-text/";
+import { PrimaryButton } from "@/components/atoms/base-no-link-button/compose";
+import { IconS } from "@/components/atoms/base-icon/compose";
+import base from "@/components/utils/base-mixin";
+export default {
+  name: "IconedTextButton",
+  mixins: [base],
+  props: {
+    type: { type: String }
+  },
+  components: {
+    IconS,
+    PrimaryButton
+  }
+};
+</script>
+<style lang="scss" module>
+.button {
+  height: 2.5rem;
+}
+.icon {
+  fill: $white;
+}
+</style>
