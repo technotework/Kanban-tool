b20075b1cc60a56c5676234e7b13d843ae3f0866
capeknote <capeknote@MacBookAir.local>
2020-03-06 12:03:23 +0900

    へっだグリッド

---
+++ src/components/templates/header-grid/index.stories.js
@@ -0,0 +1,36 @@
+import { createDefStory, createStory, tagTemp } from "@/components/utils/story-creator"
+import HeaderGrid from "./index.vue";
+
+let description = 'import HeaderGrid from "@/components/templates/header-grid/"';
+export default createDefStory("templates/HeaderGrid", HeaderGrid, description);
+
+//---------------------
+//Basic
+let setting = {
+    name: "HeaderGrid",
+    compos: { HeaderGrid: HeaderGrid },
+    props: {},
+    action: {},
+    template: tagTemp`<HeaderGrid ${'props'} ${'action'}>
+        
+    <template #unit1>
+        <div style="background-color:#ff0; width:166px; height:34px;">box</div>
+    </template>
+
+    <template #unit2>
+        <div style="background-color:#ff0; width:100%; height:24px;">box</div>
+    </template>
+
+    <template #unit3>
+        <div style="background-color:#ff0; width:145px; height:45px;">box</div>
+    </template>
+
+    <template #unit4>
+        <div style="background-color:#ff0; width:72px; height:25px;">box</div>
+    </template>
+    
+    </HeaderGrid>`
+};
+
+export const Basic = () => (createStory(setting));
+
---
+++ src/components/templates/header-grid/index.vue
@@ -0,0 +1,77 @@
+<template>
+  <div :class="$style.grid">
+    <div :class="$style.unit1">
+      <slot name="unit1" />
+    </div>
+    <div :class="$style.unit2">
+      <slot name="unit2" />
+    </div>
+    <div :class="$style.unit3">
+      <slot name="unit3" />
+    </div>
+    <div :class="$style.unit4">
+      <slot name="unit4" />
+    </div>
+  </div>
+</template>
+
+<script>
+import base from "@/components/utils/base-mixin";
+
+export default {
+  mixins: [base],
+  name: "HeaderGrid"
+};
+</script>
+<style lang="scss" module>
+.grid {
+  display: grid;
+  grid-template-columns: 288px 1fr 180px 108px;
+  grid-template-rows: auto;
+}
+.unit1 {
+  justify-content: left;
+}
+.unit2 {
+  align-self: center;
+}
+.unit3 {
+  justify-self: right;
+}
+.unit4 {
+  justify-self: right;
+  align-self: center;
+}
+
+@include responsive(970px) {
+  .grid {
+    display: grid;
+    grid-template-columns: 200px 1fr 180px 108px;
+    grid-template-rows: auto;
+  }
+}
+
+@include responsive(800px) {
+  .grid {
+    display: grid;
+    grid-template-columns: 200px 1fr 108px;
+    grid-template-rows: auto;
+  }
+  .unit3 {
+    display: none;
+  }
+}
+
+@include responsive(650px) {
+  .grid {
+    display: grid;
+    grid-template-columns: 175px 1fr 82px;
+    grid-template-rows: auto;
+  }
+  .unit3 {
+    display: none;
+  }
+}
+</style>
+
+
